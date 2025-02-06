3eaf84d8868ffe81f5396d02f0f0957265fd7890
hlopes-dev <hloops-c@outlook.com>
2025-02-06 18:55:18 +0000

    .gitignore + loop for char counting

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def main():
     with open("/home/hloops/workspace/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
     
-    words = len(file_contents.split())
+    lowered_str = file_contents.lower()
+    counter_for_chars = {}
+    
+    for c in lowered_str:
+        if c not in counter_for_chars:
+            counter_for_chars[c] = 1
+        else:
+            counter_for_chars[c] += 1
 
-    print(words)
+    print(counter_for_chars)    
 
 if __name__ == "__main__":
     main()
